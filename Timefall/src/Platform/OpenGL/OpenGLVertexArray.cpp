f3c38de0d6e505f1b5a2ce762b073198b38de87a
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2023-11-11 11:41:01 +0200

    Added a member to OpenGLVertexArrays to keep track of the index

--- Timefall/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Timefall/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -53,13 +53,18 @@ namespace Timefall
 		glBindVertexArray(m_RendererID);
 		vertexBuffer->Bind();
 
-		uint32_t index = 0;
 		const BufferLayout& layout = vertexBuffer->GetLayout();
 		for (const BufferElement& element : layout)
 		{
-			glEnableVertexAttribArray(index);
-			glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToGLBaseType(element.Type), element.Normalized ? GL_TRUE : GL_FALSE, layout.GetStride(), (const void*)(intptr_t)element.Offset);
-			index++;
+			glEnableVertexAttribArray(m_VertexBufferIndex);
+			glVertexAttribPointer(m_VertexBufferIndex, 
+				element.GetComponentCount(), 
+				ShaderDataTypeToGLBaseType(element.Type), 
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(), 
+				(const void*)(intptr_t)element.Offset);
+
+			m_VertexBufferIndex++;
 		}
 
 		m_VertexBuffers.push_back(vertexBuffer);
--- Timefall/src/Platform/OpenGL/OpenGLVertexArray.h
+++ Timefall/src/Platform/OpenGL/OpenGLVertexArray.h
@@ -20,6 +20,7 @@ namespace Timefall
 
 	private:
 		uint32_t m_RendererID;
+		uint32_t m_VertexBufferIndex = 0;
 		std::vector<Ref<VertexBuffer>> m_VertexBuffers;
 		Ref<IndexBuffer> m_IndexBuffer;
 	};
