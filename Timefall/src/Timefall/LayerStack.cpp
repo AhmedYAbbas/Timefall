12d2ae1bb0d581481d73cdde01bf069699eac009
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2023-11-11 10:09:05 +0200

    Revert "Switched to insert iterator in LayerStack instead of insert index"
    This reverts commit 57a4332e34bdf4101b7cfdbd5c2c5dc380d98a12.

--- Timefall/src/Timefall/LayerStack.cpp
+++ Timefall/src/Timefall/LayerStack.cpp
@@ -5,7 +5,7 @@ namespace Timefall
 {
 	LayerStack::LayerStack()
 	{
-		m_LayerInsert = m_Layers.begin();
+		
 	}
 
 	LayerStack::~LayerStack()
@@ -16,8 +16,10 @@ namespace Timefall
 
 	void LayerStack::PushLayer(Layer* layer)
 	{
-		m_LayerInsert = m_Layers.emplace(m_LayerInsert, layer);
+		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
 		layer->OnAttach();
+
+		m_LayerInsertIndex++;
 	}
 
 	void LayerStack::PushOverlay(Layer* overlay)
@@ -32,8 +34,7 @@ namespace Timefall
 		if (it != m_Layers.end())
 		{
 			m_Layers.erase(it);
-			m_LayerInsert--;
-			layer->OnDetach();
+			m_LayerInsertIndex--;
 		}
 	}
 
@@ -41,9 +42,6 @@ namespace Timefall
 	{
 		const auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
 		if (it != m_Layers.end())
-		{
 			m_Layers.erase(it);
-			overlay->OnDetach();
-		}
 	}
 }
