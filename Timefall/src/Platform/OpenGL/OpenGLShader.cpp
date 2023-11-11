36a6cb2970c04dc4718f5f3eb8a2326fb3209b80
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2023-11-11 14:23:52 +0200

    Fixed edge case in OpenGLShader::Preprocess Logic

--- Timefall/src/Platform/OpenGL/OpenGLShader.cpp
+++ Timefall/src/Platform/OpenGL/OpenGLShader.cpp
@@ -73,8 +73,9 @@ namespace Timefall
 			TF_CORE_ASSERT(ShaderTypeFromString(type), "Invalid shader type specifier!");
 
 			size_t nextLinePos = source.find_first_not_of("\r\n", eol);
+			HZ_CORE_ASSERT(nextLinePos != std::string::npos, "Syntax error");
 			pos = source.find(typeToken, nextLinePos);
-			shaderSources[ShaderTypeFromString(type)] = source.substr(nextLinePos, pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos));
+			shaderSources[ShaderTypeFromString(type)] = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
 		}
 
 		return shaderSources;
