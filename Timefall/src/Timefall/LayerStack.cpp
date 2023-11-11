ce3affb24266661def7dd8365114b8966abd2ea6
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2023-11-11 10:16:41 +0200

    Called OnDetach when popping from LayerStack

--- Timefall/src/Timefall/LayerStack.cpp
+++ Timefall/src/Timefall/LayerStack.cpp
@@ -4,8 +4,7 @@
 namespace Timefall
 {
 	LayerStack::LayerStack()
-	{
-		
+	{	
 	}
 
 	LayerStack::~LayerStack()
@@ -33,6 +32,7 @@ namespace Timefall
 		const auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
 		if (it != m_Layers.end())
 		{
+			layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
 		}
@@ -42,6 +42,9 @@ namespace Timefall
 	{
 		const auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
 		if (it != m_Layers.end())
+		{
+			overlay->OnDetach();
 			m_Layers.erase(it);
+		}
 	}
 }
