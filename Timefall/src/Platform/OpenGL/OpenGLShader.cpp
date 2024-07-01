f69256155d71097efffd740400de6392fc2abf46
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2024-07-01 12:08:23 +0300

    Added shader file reading check

--- Timefall/src/Platform/OpenGL/OpenGLShader.cpp
+++ Timefall/src/Platform/OpenGL/OpenGLShader.cpp
@@ -52,10 +52,18 @@ namespace Timefall
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+				TF_CORE_ERROR("Could not read from file '{0}'", filepath);
+			}
 		}
 		else
 		{
