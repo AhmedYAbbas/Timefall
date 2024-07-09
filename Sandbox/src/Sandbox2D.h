9c2def47778d65d23b1e62e268c4e63974296b38
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2024-07-09 10:18:33 +0300

    Added performance timing

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -15,11 +15,21 @@ public:
 	virtual void OnEvent(Timefall::Event& e) override;
 	virtual void OnImGuiRender() override;
 private:
-	Timefall::Ref<Timefall::Shader> m_FlatColorShader;
-	Timefall::Ref<Timefall::VertexArray> m_SquareVA;
-	glm::vec4 m_SqaureColor = {0.2f, 0.3f, 0.8f, 1.0f};
-
 	Timefall::OrthographicCameraController m_CameraController;
 
+	// Temp
+	Timefall::Ref<Timefall::Shader> m_FlatColorShader;
+	Timefall::Ref<Timefall::VertexArray> m_SquareVA;
+
 	Timefall::Ref<Timefall::Texture2D> m_CheckerboardTexture;
+
+	glm::vec4 m_SqaureColor = {0.2f, 0.3f, 0.8f, 1.0f};
+
+	struct ProfileResult
+	{
+		const char* Name;
+		float Time;
+	};
+
+	std::vector<ProfileResult> m_ProfileResults;
 };
