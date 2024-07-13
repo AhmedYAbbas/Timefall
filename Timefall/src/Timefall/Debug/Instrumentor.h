4cc7e3df438df1d67955e260724c7ed160759ee7
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2024-07-13 21:18:01 +0300

    Instrumetation fixes

--- Timefall/src/Timefall/Debug/Instrumentor.h
+++ Timefall/src/Timefall/Debug/Instrumentor.h
@@ -4,7 +4,7 @@
 #include <chrono>
 #include <algorithm>
 #include <fstream>
-
+#include <string>
 #include <thread>
 
 namespace Timefall
@@ -13,7 +13,7 @@ namespace Timefall
     {
         std::string Name;
         long long Start, End;
-        size_t ThreadID;
+        std::thread::id ThreadID;
     };
 
     struct InstrumentationSession
@@ -25,57 +25,70 @@ namespace Timefall
     {
     public:
         Instrumentor()
-            : m_CurrentSession(nullptr), m_ProfileCount(0)
+            : m_CurrentSession(nullptr)
         {
         }
 
         void BeginSession(const std::string& name, const std::string& filepath = "results.json")
         {
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentSession)
+            {
+                // If there is already a current session, then close it before beginning new one.
+                // Subsequent profiling output meant for the original session will end up in the
+                // newly opened session instead.  That's better than having badly formatted
+                // profiling output.
+                if (Log::GetCoreLogger())
+                { // Edge case: BeginSession() might be before Log::Init()
+                    TF_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+                }
+                InternalEndSession();
+            }
+
             m_OutputStream.open(filepath);
-            WriteHeader();
-            m_CurrentSession = new InstrumentationSession {name};
+            if (m_OutputStream.is_open())
+            {
+                m_CurrentSession = new InstrumentationSession({name});
+                WriteHeader();
+            }
+            else
+            {
+                if (Log::GetCoreLogger())
+                { // Edge case: BeginSession() might be before Log::Init()
+                    TF_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+                }
+            }
         }
 
         void EndSession()
         {
-            WriteFooter();
-            m_OutputStream.close();
-            delete m_CurrentSession;
-            m_CurrentSession = nullptr;
-            m_ProfileCount = 0;
+            std::lock_guard lock(m_Mutex);
+            InternalEndSession();
         }
 
         void WriteProfile(const ProfileResult& result)
         {
-            if (m_ProfileCount++ > 0)
-                m_OutputStream << ",";
+            std::stringstream json;
 
             std::string name = result.Name;
             std::replace(name.begin(), name.end(), '"', '\'');
 
-            m_OutputStream << "{";
-            m_OutputStream << "\"cat\":\"function\",";
-            m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-            m_OutputStream << "\"name\":\"" << name << "\",";
-            m_OutputStream << "\"ph\":\"X\",";
-            m_OutputStream << "\"pid\":0,";
-            m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-            m_OutputStream << "\"ts\":" << result.Start;
-            m_OutputStream << "}";
+            json << ",{";
+            json << "\"cat\":\"function\",";
+            json << "\"dur\":" << (result.End - result.Start) << ',';
+            json << "\"name\":\"" << name << "\",";
+            json << "\"ph\":\"X\",";
+            json << "\"pid\":0,";
+            json << "\"tid\":" << result.ThreadID << ",";
+            json << "\"ts\":" << result.Start;
+            json << "}";
 
-            m_OutputStream.flush();
-        }
-
-        void WriteHeader()
-        {
-            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
-            m_OutputStream.flush();
-        }
-
-        void WriteFooter()
-        {
-            m_OutputStream << "]}";
-            m_OutputStream.flush();
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentSession)
+            {
+                m_OutputStream << json.str();
+                m_OutputStream.flush();
+            }
         }
 
         static Instrumentor& Get()
@@ -85,9 +98,35 @@ namespace Timefall
         }
 
     private:
+        void WriteHeader()
+        {
+            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
+            m_OutputStream.flush();
+        }
+
+        void WriteFooter()
+        {
+            m_OutputStream << "]}";
+            m_OutputStream.flush();
+        }
+
+        // Note: you must already own lock on m_Mutex before
+        // calling InternalEndSession()
+        void InternalEndSession()
+        {
+            if (m_CurrentSession)
+            {
+                WriteFooter();
+                m_OutputStream.close();
+                delete m_CurrentSession;
+                m_CurrentSession = nullptr;
+            }
+        }
+
+    private:
+        std::mutex m_Mutex;
         InstrumentationSession* m_CurrentSession;
         std::ofstream m_OutputStream;
-        int m_ProfileCount;
     };
 
     class InstrumentationTimer
@@ -112,8 +151,7 @@ namespace Timefall
             long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
             long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
-            size_t threadID = std::hash<std::thread::id> {}(std::this_thread::get_id());
-            Instrumentor::Get().WriteProfile({m_Name, start, end, threadID});
+            Instrumentor::Get().WriteProfile({m_Name, start, end, std::this_thread::get_id()});
 
             m_Stopped = true;
         }
