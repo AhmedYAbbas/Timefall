eecb45ac80fb2e0a5ea7781ffd2250ef032b86dd
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2024-07-22 14:59:55 +0300

    Added a comment regarding the opengl index buffer creation

--- Timefall/src/Platform/OpenGL/OpenGLBuffer.cpp
+++ Timefall/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -66,6 +66,8 @@ namespace Timefall
 	{
 		TF_PROFILE_FUNCTION();
 
+		// GL_ELEMENT_ARRAY_BUFFER is not valid without an actively bound VAO
+		// Binding with GL_ARRAY_BUFFER allows the data to be loaded regardless of VAO state. 
 		glCreateBuffers(1, &m_RendererID);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
