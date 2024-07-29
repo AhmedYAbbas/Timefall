1c302471afacebe2c2da68fe8d475187c0c22657
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2024-07-29 10:46:28 +0300

    Fixed IndexCount selection in OpenGLRendererAPI::DrawIndexed

--- Timefall/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Timefall/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -70,7 +70,7 @@ namespace Timefall
     {
         TF_PROFILE_FUNCTION();
 
-        uint32_t count = indexCount ? vertexArray->GetIndexBuffer()->GetCount() : indexCount;
+        uint32_t count = indexCount ? indexCount : vertexArray->GetIndexBuffer()->GetCount();
         glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
         glBindTexture(GL_TEXTURE_2D, 0);
     }
