fd54faa7ea35a6407e376f39d1d129fe01a5ae44
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2024-07-29 11:33:18 +0300

    Optimization in Instrumentor.h

--- Timefall/src/Timefall/Core/Log.cpp
+++ Timefall/src/Timefall/Core/Log.cpp
@@ -13,7 +13,7 @@ namespace Timefall
 	{
 		std::vector<spdlog::sink_ptr> logSinks;
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Timefall.log", true));
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Timefall-Log.log", true));
 
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
--- Timefall/src/Timefall/Debug/Instrumentor.h
+++ Timefall/src/Timefall/Debug/Instrumentor.h
@@ -74,14 +74,11 @@ namespace Timefall
         {
             std::stringstream json;
 
-            std::string name = result.Name;
-            std::replace(name.begin(), name.end(), '"', '\'');
-
             json << std::setprecision(3) << std::fixed;
             json << ",{";
             json << "\"cat\":\"function\",";
             json << "\"dur\":" << result.ElapsedTime.count() << ',';
-            json << "\"name\":\"" << name << "\",";
+            json << "\"name\":\"" << result.Name << "\",";
             json << "\"ph\":\"X\",";
             json << "\"pid\":0,";
             json << "\"tid\":" << result.ThreadID << ",";
@@ -164,6 +161,36 @@ namespace Timefall
         std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
         bool m_Stopped;
     };
+
+    namespace InstrumentorUtils
+    {
+
+        template <size_t N>
+        struct ChangeResult
+        {
+            char Data[N];
+        };
+
+        template <size_t N, size_t K>
+        constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+        {
+            ChangeResult<N> result = {};
+
+            size_t srcIndex = 0;
+            size_t dstIndex = 0;
+            while (srcIndex < N)
+            {
+                size_t matchIndex = 0;
+                while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+                    matchIndex++;
+                if (matchIndex == K - 1)
+                    srcIndex += matchIndex;
+                result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+                srcIndex++;
+            }
+            return result;
+        }
+    }
 }
 
 #define TF_PROFILE 1
@@ -176,7 +203,7 @@ namespace Timefall
         #define TF_FUNC_SIG __PRETTY_FUNCTION__
     #elif defined(__DMC__) && (__DMC__ >= 0x810)
         #define TF_FUNC_SIG __PRETTY_FUNCTION__
-    #elif defined(__FUNCSIG__)
+    #elif (defined(__FUNCSIG__) || (_MSC_VER))
         #define TF_FUNC_SIG __FUNCSIG__
     #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
         #define TF_FUNC_SIG __FUNCTION__
@@ -190,9 +217,10 @@ namespace Timefall
         #define TF_FUNC_SIG "TF_FUNC_SIG unknown!"
     #endif
 
-    #define TF_PROFILE_BEGIN_SESSION(name, filepath) Timefall::Instrumentor::Get().BeginSession(name, filepath)
-    #define TF_PROFILE_END_SESSION() Timefall::Instrumentor::Get().EndSession()
-    #define TF_PROFILE_SCOPE(name) Timefall::InstrumentationTimer timer##__LINE__(name)
+    #define TF_PROFILE_BEGIN_SESSION(name, filepath) ::Timefall::Instrumentor::Get().BeginSession(name, filepath)
+    #define TF_PROFILE_END_SESSION() ::Timefall::Instrumentor::Get().EndSession()
+    #define TF_PROFILE_SCOPE(name) constexpr auto fixedName = ::Timefall::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+									::Timefall::InstrumentationTimer timer##__LINE__(fixedName.Data)
     #define TF_PROFILE_FUNCTION() TF_PROFILE_SCOPE(TF_FUNC_SIG)
 #else
     #define TF_PROFILE_BEGIN_SESSION(name, filepath)
