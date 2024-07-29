580ae439d671d4752de86992505113f848cf525d
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2024-07-29 11:06:04 +0300

    Fixed incorrect cases in BufferElement::GetComponentCount

--- Timefall/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Timefall/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -16,7 +16,7 @@ namespace Timefall
 			case ShaderDataType::Float4:   return GL_FLOAT;
 			case ShaderDataType::Mat3:     return GL_FLOAT;
 			case ShaderDataType::Mat4:     return GL_FLOAT;
-			case ShaderDataType::Int:	     return GL_INT;
+			case ShaderDataType::Int:	   return GL_INT;
 			case ShaderDataType::Int2:     return GL_INT;
 			case ShaderDataType::Int3:     return GL_INT;
 			case ShaderDataType::Int4:     return GL_INT;
@@ -67,15 +67,49 @@ namespace Timefall
 		const BufferLayout& layout = vertexBuffer->GetLayout();
 		for (const BufferElement& element : layout)
 		{
-			glEnableVertexAttribArray(m_VertexBufferIndex);
-			glVertexAttribPointer(m_VertexBufferIndex, 
-				element.GetComponentCount(), 
-				ShaderDataTypeToGLBaseType(element.Type), 
-				element.Normalized ? GL_TRUE : GL_FALSE, 
-				layout.GetStride(), 
-				(const void*)element.Offset);
-
-			m_VertexBufferIndex++;
+			switch (element.Type)
+			{
+				case ShaderDataType::Float:
+				case ShaderDataType::Float2:
+				case ShaderDataType::Float3:
+				case ShaderDataType::Float4:
+				case ShaderDataType::Int:
+				case ShaderDataType::Int2:
+				case ShaderDataType::Int3:
+				case ShaderDataType::Int4:
+				case ShaderDataType::Bool:
+				{
+					glEnableVertexAttribArray(m_VertexBufferIndex);
+					glVertexAttribPointer(m_VertexBufferIndex,
+						element.GetComponentCount(),
+						ShaderDataTypeToGLBaseType(element.Type),
+						element.Normalized ? GL_TRUE : GL_FALSE,
+						layout.GetStride(),
+						(const void*)element.Offset);
+					m_VertexBufferIndex++;
+					break;
+				}
+				case ShaderDataType::Mat3:
+				case ShaderDataType::Mat4:
+				{
+					uint8_t count = element.GetComponentCount();
+					for (uint8_t i = 0; i < count; i++)
+					{
+						glEnableVertexAttribArray(m_VertexBufferIndex);
+						glVertexAttribPointer(m_VertexBufferIndex,
+							count,
+							ShaderDataTypeToGLBaseType(element.Type),
+							element.Normalized ? GL_TRUE : GL_FALSE,
+							layout.GetStride(),
+							(const void*)(sizeof(float) * count * i));
+						glVertexAttribDivisor(m_VertexBufferIndex, 1);
+						m_VertexBufferIndex++;
+					}
+					break;
+				}
+				default:
+					TF_CORE_ASSERT(false, "Unknown ShaderDataType!");
+			}
 		}
 
 		m_VertexBuffers.push_back(vertexBuffer);
